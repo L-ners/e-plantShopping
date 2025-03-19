7b6812679fc9fd7ec708ff6d07b248bfe2bf9f65
L-ners <leonardo.berrios42@gmail.com>
2025-03-19 19:03:10 -0400

    Fixed Redux store integration and improved index.js

--- src/store.js
+++ src/store.js
@@ -1,8 +1,12 @@
-import { configureStore } from '@reduxjs/toolkit';
-import cartReducer from "./CartSlice";
- const store = configureStore({
-    reducer: {
-        cart: cartReducer,
-    },
+import { configureStore } from "@reduxjs/toolkit";
+import cartReducer from "./CartSlice"; // Import the cart reducer
+
+// Configure Redux store
+const store = configureStore({
+  reducer: {
+    cart: cartReducer, // Assign the cartReducer to the cart slice
+  },
 });
-export default store
+
+export default store; // Export the store to be used in the application
+
