dff15ee382b98da011ddefe5763ff89b0c229a27
L-ners <leonardo.berrios42@gmail.com>
2025-03-19 19:05:35 -0400

    Set up global Redux store in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import store from './store'; // Ensure correct import
+import { Provider } from 'react-redux'; // Import Redux Provider
+import store from './store'; // Import the Redux store
 import App from './App';
 import './index.css';
 
-// Creating the root element and rendering the App within the Redux Provider
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(
+// Create root and render the application with Redux store
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>
 );
+
