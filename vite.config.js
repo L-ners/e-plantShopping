e172c4028d02ed78857620016760b1d450f8b57f
L-ners <leonardo.berrios42@gmail.com>
2025-03-19 20:12:53 -0400

    Fix JSX loading issue for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/e-plantShopping/", // Ensure this matches your GitHub repo name
+  base: "/e-plantShopping", // Ensure this matches your GitHub repo name
   plugins: [react()],
   resolve: {
     extensions: [".js", ".jsx"], // Ensure JSX files are properly resolved
