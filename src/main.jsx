41568bf853b8238bbfa195ab68e15366ee5681eb
L-ners <leonardo.berrios42@gmail.com>
2025-03-19 20:06:32 -0400

    Fixed Vite config and  for main.jsx deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App"; // ✅ Remove .jsx extension
+import App from "./App"; // ✅ No .jsx extension
 import { Provider } from "react-redux";
 import store from "./store";
 
@@ -11,4 +11,5 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     </Provider>
   </React.StrictMode>
 );
+
 
